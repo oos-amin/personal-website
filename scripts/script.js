f2a80024a71249185f081ac0480c061231bbc7d4
Amin <amin47moeini@gmail.com>
2025-08-24 19:14:38 +0330

    feat: add menu interaction scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 'use strict'
 const menu = document.querySelector('.nav__toggle-icon');
+const menuList = document.querySelector('.nav__list');
 
 menu.addEventListener('click', function () {
     menu.classList.toggle('nav__toggle-icon--open');
+    menuList.classList.toggle('nav__list--open')
 });
