adc36569ecb5d4f1ca6d91e11b4ae2767aa15dc4
Amin <amin47moeini@gmail.com>
2025-08-24 21:54:22 +0330

    feat: add overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 'use strict'
 const menu = document.querySelector('.nav__toggle-icon');
 const menuList = document.querySelector('.nav__list');
+const overlay = document.querySelector('.overlay');
 
 menu.addEventListener('click', function () {
     menu.classList.toggle('nav__toggle-icon--open');
     menuList.classList.toggle('nav__list--open')
+    overlay.classList.toggle('overlay--show');
 });
